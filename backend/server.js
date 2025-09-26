be1fd18d1c01b8df468aecef757e23c542d7a3e0
Kaksophat <160870070+Kaksophat@users.noreply.github.com>
2025-09-26 08:54:38 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",   
+  origin: "https://mern-taskmanagement-frontend.onrender.com",   
   credentials: true                  
 }));
 app.use(express.urlencoded({ extended: true }));
